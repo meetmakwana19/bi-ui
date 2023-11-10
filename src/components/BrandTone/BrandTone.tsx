30413694b25cb6a6de8fae4ce58c63185a741675
CodeXOmsai <omsaix100@gmail.com>
2023-11-10 17:24:26 +0530

    refactor: Added button in add tone

--- src/components/BrandTone/BrandTone.tsx
+++ src/components/BrandTone/BrandTone.tsx
@@ -1,21 +1,21 @@
-import { Help } from '@contentstack/venus-components';
 import React from 'react'
+import './BrandTone.css'
+import { Button, Help } from '@contentstack/venus-components'
 
 interface BrandToneProps {}
 
 const BrandTone: React.FC<BrandToneProps> = () => {
   return (
     <div className="brand-tone-style">
-      {/* <SideNav /> */}
-      {/* <Header /> */}
-      <div className='tone-header'>
-        <h1>Brand Tone</h1>
-        <Help 
-          text="Your tone enables Contentstack to align content with your brand's voice and personality."
-        />
-      </div>
-      <div className="voice-tone">
-        <h3>Voice & Tone</h3>
+      <div className='heading-button'>
+
+        <section className='heading'>
+          <h2>User Tone <Help text="User Tone empowers Contentstack to align content with your brand's voice and personality." type="primary" alignment="right" />
+          </h2>
+        </section>
+
+          <Button icon="AddPlus" buttonType="primary" size='regular' >Add Tone</Button>
+
       </div>
     </div>
   );
