5d23273600092f08f32dfd3809fa800545d4f19c
CodeXOmsai <omsaix100@gmail.com>
2023-11-15 12:28:21 +0530

    added: Table outline added

--- src/components/BrandTone/BrandTone.tsx
+++ src/components/BrandTone/BrandTone.tsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import './BrandTone.css'
-import { Button, Help } from '@contentstack/venus-components'
+import { Button, Help, Table } from '@contentstack/venus-components'
 
 interface BrandToneProps {}
 
 const BrandTone: React.FC<BrandToneProps> = () => {
+
+  const tableProps = {
+    columns: [
+      {Header: 'Name'},
+      {Header: 'Created By'},
+      {Header: 'Modified at'},
+      {Header: 'Tags'},
+    ] ,
+    data: [] ,
+    uniqueKey: 'id' ,
+    totalCounts: 3,
+  }
   return (
     <div className="brand-tone-style">
       <div className='heading-button'>
 
-        <section className='heading'>
-          <h2>User Tone <Help text="User Tone empowers Contentstack to align content with your brand's voice and personality." type="primary" alignment="right" />
-          </h2>
-        </section>
+        <h2>User Tone <Help text="User Tone empowers Contentstack to align content with your brand's voice and personality." type="primary" alignment="right" />
+        </h2>
 
-          <Button icon="AddPlus" buttonType="primary" size='regular' >Add Tone</Button>
+        <Button icon="AddPlus" buttonType="primary" size='regular' >Add Tone</Button>
 
       </div>
+
+      <Table {...tableProps}/>
     </div>
   );
 }
