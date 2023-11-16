0d5d04e0a730d4e5ed4a055c39c03bb984b59711
Meet Makwana <meetmakwana203@gmail.com>
2023-11-16 11:36:11 +0530

    feat: data loaded in InfiniteScrollTable

--- src/components/BrandVoice/TableEntries.tsx
+++ src/components/BrandVoice/TableEntries.tsx
@@ -5,27 +5,33 @@ function TableEntries() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [totalCounts, setTotalCounts] = useState(null);
+  const [itemStatusMap, setItemStatusMap] = useState({});
 
   const columns = [
     {
       Header: 'Name',
       id: 'name',
-      accessor: (data: any) => (
-        <div>{data.title}</div>
-      ),
+      accessor: (data: any) => {
+        // console.log("getting data : ", data)
+        return (
+          <p>{data.name}</p>
+        )
+      },
     },
     {
       Header: 'Created By',
       id: 'created_by',
-      accessor: (data: any) => (
-        <div>{data.completed}</div>
-      ),
+      accessor: (data: any) => {
+        return (
+          <div> {data.email}</div >
+        )
+      },
     },
     {
       Header: 'Created At',
       id: 'created_at',
       accessor: (data: any) => (
-        <div>{data.userId}</div>
+        <div>{data.id}</div>
       ),
     },
   ];
@@ -34,14 +40,11 @@ function TableEntries() {
     try {
       setLoading(true);
 
-      console.log("=========", startIndex, " and ", limit);
-      
-      // Using JSONPlaceholder as an example API
-      const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_page=${startIndex / limit + 1}&_limit=${limit}`);
+      const response = await fetch(`https://jsonplaceholder.typicode.com/comments?_page=${startIndex / limit + 1}&_limit=${limit}`);
       const responseData = await response.json();
 
       console.log("respnse : ", responseData);
-      
+
       setLoading(false);
       setData([...data, ...responseData]);
       setTotalCounts(responseData.length);
@@ -54,9 +57,16 @@ function TableEntries() {
     try {
       setLoading(true);
 
-      // Using JSONPlaceholder as an example API
-      const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_page=${startIndex / limit + 1}&_limit=${limit}`);
+      const response = await fetch(`https://jsonplaceholder.typicode.com/comments?_page=${startIndex / limit + 1}&_limit=${limit}`);
       const responseData = await response.json();
+
+      // Updating item statuses and simulating loaded states
+      const updatedItemStatusMap = { ...itemStatusMap };
+      for (let index = startIndex; index <= stopIndex; index++) {
+        updatedItemStatusMap[index] = 'loaded';
+      }
+
+      setItemStatusMap(updatedItemStatusMap);
 
       setLoading(false);
       setData([...data, ...responseData]);
@@ -75,14 +85,13 @@ function TableEntries() {
         loading={loading}
         totalCounts={totalCounts}
         loadMoreItems={loadMoreItems}
-        itemStatusMap={{}}
+        itemStatusMap={itemStatusMap}
         minBatchSizeToFetch={10}
         initialSortBy={[]} // No initial sorting for simplicity
-        viewSelector={false} // Hide the view selector for simplicity
-        columnSelector={false} // Hide the column selector for simplicity
-        searchPlaceholder="Search" // Provide a placeholder for the search input
-        canSearch={true} // Enable search functionality
-        canRefresh={true} // Enable refresh functionality
+        viewSelector={true} // Settings icon
+        searchPlaceholder="Search by name, description or tags" 
+        canSearch={true} 
+        canRefresh={true}
       />
     </>
   )
