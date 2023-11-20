de6fa60e20298b4f6cf6f7903314b9097338ec05
Meet Makwana <meetmakwana203@gmail.com>
2023-11-20 15:41:44 +0530

    fix: brandvoice page styling

--- src/components/BrandVoice/BrandVoice.tsx
+++ src/components/BrandVoice/BrandVoice.tsx
@@ -1,124 +1,13 @@
-import React, { useState } from "react";
-import TextModal from "../Modals/KnowledgeBase/TextModal";
-import FileModal from "../Modals/KnowledgeBase/FileModal";
-import LinkModal from "../Modals/KnowledgeBase/LinkModal";
+import React from "react";
 import TableEntries from "./TableEntries";
 
 interface BrandVoiceProps {
-  // toggleBrandVoiceClick: () => void;
-  // isBrandVoiceOpen: boolean;
 }
 
 const BrandVoice: React.FC<BrandVoiceProps> = () => {
-  const [modalShow, setModalShow] = useState(false);
-  const [fileModalShow, setFileModalShow] = useState(false);
-  const [linkModalShow, setLinkModalShow] = useState(false)
   return (
     <div className="main-area">
-      <div className="know-base py-3 px-4">
-        <div className="d-flex justify-content-between">
-          <div
-            className="modal fade"
-            id="menuModal"
-            tabIndex={-1}
-            aria-labelledby="KBModalLabel"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog modal-dialog-centered modal-lg">
-              <div className="modal-content">
-                <div className="modal-header align-items-start flex-row justify-content-between">
-                  <div>
-                    <h1 className="modal-title fs-5" id="KBModalLabel">
-                      Add to knowledge base
-                    </h1>
-                    <p className="bold-gray m-0">
-                      Give Brand Intelligence facts to more accurately write
-                      about any topic.
-                    </p>
-                  </div>
-                  <button
-                    type="button"
-                    className="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                  ></button>
-                </div>
-                <div className="modal-body">
-                  <h6 style={{ color: "#000000c9" }}>
-                    What type of content would you like to give to Brand
-                    Intelligence? Please select one that applies.
-                  </h6>
-                  <div className="d-flex p-3 square-btn-div">
-                    <button
-                      className="square-btn p-4 border text-center rounded-4"
-                      type="button"
-                      onClick={() => setModalShow(true)}
-                      data-bs-dismiss="modal"
-                    >
-                      <i
-                        className="bi bi-text-left fs-1"
-                        style={{ color: "#675af2" }}
-                      ></i>
-                      <p className="fw-semibold">From text</p>
-                      <p style={{ fontSize: "0.85rem" }}>
-                        Write or copy paste text.
-                      </p>
-                    </button>
-                    <button
-                      className="square-btn p-4 border text-center rounded-4 btn mx-4"
-                      type="button"
-                      onClick={() => setFileModalShow(true)}
-                      data-bs-dismiss="modal"
-                    >
-                      <i
-                        className="bi bi-file-earmark-text fs-1"
-                        style={{ color: "#675af2" }}
-                      ></i>
-                      <p className="fw-semibold">Upload file</p>
-                      <p style={{ fontSize: "0.85rem" }}>.pdf supported</p>
-                    </button>
-                    <button
-                      className="square-btn p-4 border text-center rounded-4"
-                      type="button"
-                      onClick={() => setLinkModalShow(true)}
-                      data-bs-dismiss="modal"
-                    >
-                      <i
-                        className="bi bi-link-45deg fs-1"
-                        style={{ color: "#675af2" }}
-                      ></i>
-                      <p className="fw-semibold">Enter URL</p>
-                      <p style={{ fontSize: "0.85rem" }}>
-                        Brand Intelligence will scan a site.
-                      </p>
-                    </button>
-                  </div>
-                  <p className="bold-gray m-0">
-                    You must own or have permission to use any content you
-                    submit to Brand Intelligence. Using Brand Intelligence to
-                    violate someone else's rights is a violation of our Terms of
-                    Service.
-                  </p>
-                  <TextModal
-                    show={modalShow}
-                    onHide={() => setModalShow(false)}
-                  />
-                  <FileModal
-                    show={fileModalShow}
-                    onHide={() => setFileModalShow(false)}
-                  />
-                  <LinkModal
-                    show={linkModalShow}
-                    onHide={() => setLinkModalShow(false)}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <TableEntries />
-      </div>
+      <TableEntries />
     </div>
   );
 }
