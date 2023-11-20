7ec4e9164cf0deaabde1433ba9e1a5a41d0ea7b5
Meet Makwana <meetmakwana203@gmail.com>
2023-11-20 14:31:19 +0530

    feat: routes with a new page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,31 +4,36 @@ import SideNav from "./SideNav/SideNav";
 import { Outlet, Route, Routes } from "react-router-dom";
 import BrandVoice from "./BrandVoice/BrandVoice";
 import UserTone from "./UserTone/UserTone";
+import AddEntry from "./BrandVoice/Forms/AddEntry";
 
 interface MainProps { }
 
-const Layout = () => {
-    console.log("yooooooo");
-
+const Layout = ({ children }: React.PropsWithChildren<{}>) => {
     return (
         <>
-            <Header />
-            <Outlet />
+            <div className="grid">
+                <SideNav />
+                <Header />
+                {children}
+                <Outlet />
+            </div>
         </>
-    )
-}
+    );
+};
+
 const Main: React.FC<MainProps> = () => {
     return (
-        <div className="grid">
-            <SideNav />
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route path="/" element={<BrandVoice />} />
-                    <Route path="/user" element={<UserTone />} />
-                </Route>
-            </Routes>
-        </div>
-    )
-}
+        <Routes>
+            {/* ROUTE PART-1 main page */}
+            <Route path="/" element={<Layout />}>
+                <Route index element={<BrandVoice />} />
+                <Route path="/user" element={<UserTone />} />
+            </Route>
 
-export default Main
+            {/* ROUTE PART-2 forms */}
+            <Route path="new_entry" element={<AddEntry />} />
+        </Routes>
+    );
+};
+
+export default Main;
