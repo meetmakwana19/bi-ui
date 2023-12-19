b77bb838acd507eab9c0a9dd7a46e893af7f4d90
Meet Makwana <meetmakwana203@gmail.com>
2023-12-19 15:54:35 +0530

    fix: history prop passed to Route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { IMicroAppsObj } from './app/common/models/microAppObj.ts'
-import { createBrowserHistory, History } from 'history';
+import { createBrowserHistory } from 'history';
 import { ErrorBoundary } from './app/common/components/ErrorBoundary/index.tsx'
 
 // ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -19,8 +19,8 @@ declare global {
   interface Window {
     renderIntelligenceHub: (
       containerId?: string,
-      histroy?: History,
-      // check this
+      // histroy?: History,
+      histroy?: any, // typing it as `any` makes the history prop work on the Router component.  
       microAppsObj?: IMicroAppsObj,
     ) => void;
     DD_RUM?: any;
@@ -34,10 +34,10 @@ window.renderIntelligenceHub = (
     relativeUrl: "/intelligencehub",
     leftsidebarContainerDom: "backdrop",
     org_uid: null,
-    token: "12345678",
+    token: null,
     currentUser: {},
     currentOrganization: {},
-    // check this
+    project_id: "12345678",
   }
 ) => {
   ReactDOM.render(
